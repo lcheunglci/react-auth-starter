a033d3d0da142f838d041b8066f5dec19d1176d5
Lawrence LCI <lcheunglci@gmail.com>
2022-03-10 23:23:19 -0800

    Add sign up page

--- front-end/src/pages/SignUpPage.js
+++ front-end/src/pages/SignUpPage.js
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-export const LogInPage = () => {
+export const SignUpPage = () => {
     const [errorMessage, setErrorMessage] = useState('');
     const [emailValue, setEmailValue] = useState('');
     const [passwordValue, setPasswordValue] = useState('');
+    const [confirmPasswordValue, setConfirmPasswordValue] = useState('');
     const history = useHistory();
-    const onLogInClicked = async () => {
-        alert('not implemented yet');
+    const onSignUpClicked = async () => {
+        alert('sign up not implemented yet');
     }
 
     return (
         <div className="content-container">
-            <h1>Log In</h1>
+            <h1>Sign Up</h1>
             {errorMessage && <div className="fail">{errorMessage}</div> }
             <input
                 value={emailValue}
@@ -25,14 +26,21 @@ export const LogInPage = () => {
                 type="password"
                 placeholder="password"
                  />
+            <input
+                value={confirmPasswordValue}
+                onChange={e => setConfirmPasswordValue(e.target.value)}
+                type="password"
+                placeholder="password"
+                 />
             <hr />
             <button
-                disabled={!emailValue || !passwordValue}
-                onClick={onLogInClicked}>Log In</button>
+                disabled={!emailValue || !passwordValue ||
+                    passwordValue !== confirmPasswordValue}
+                onClick={onSignUpClicked}>Sign Up</button>
             <button onClick={() => history.push('/forgot-password')}>
                 Forgot your password?
             </button>
-            <button onClick={() => history.push('/signup>')} >Don't have an account? Sign up</button>
+            <button onClick={() => history.push('/login>')} >Already have an account? Log In</button>
         </div>
     )
 };
